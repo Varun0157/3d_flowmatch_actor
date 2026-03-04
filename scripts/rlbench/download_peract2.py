fe95db18b19d923351fbed1fb0ebb4d4c74d4bce
Varun Edachali <varun.edachali@gmail.com>
2026-03-04 23:22:38 +0530

    download a subset of raw data

--- scripts/rlbench/download_peract2.py
+++ scripts/rlbench/download_peract2.py
@@ -21,18 +21,7 @@ LINK = 'https://dataset.cs.washington.edu/fox/bimanual/image_size_256'
 
 tasks = [
     'bimanual_push_box',
-    'bimanual_lift_ball',
-    'bimanual_dual_push_buttons',
     'bimanual_pick_plate',
-    'bimanual_put_item_in_drawer',
-    'bimanual_put_bottle_in_fridge',
-    'bimanual_handover_item',
-    'bimanual_pick_laptop',
-    'bimanual_straighten_rope',
-    'bimanual_sweep_to_dustpan',
-    'bimanual_lift_tray',
-    'bimanual_handover_item_easy',
-    'bimanual_take_tray_out_of_oven'
 ]
 
 
@@ -43,26 +32,22 @@ for split in ['train', 'val']:
         if os.path.exists(f'{STORE_PATH}/{split}/{task}'):
             continue
         subprocess.run(
-            f"wget --no-check-certificate {LINK}/{task}.{split}.squashfs",
-            shell=True,
-            capture_output=True, text=True, check=True,
+            f"wget -c --no-check-certificate {LINK}/{task}.{split}.squashfs",
+            shell=True, check=True,
             cwd=f"{STORE_PATH}/{split}"
         )
         subprocess.run(
             f"unsquashfs {task}.{split}.squashfs",
-            shell=True,
-            capture_output=True, text=True, check=True,
+            shell=True, check=True,
             cwd=f"{STORE_PATH}/{split}"
         )
         subprocess.run(
             f"mv squashfs-root/ {task}",
-            shell=True,
-            capture_output=True, text=True, check=True,
+            shell=True, check=True,
             cwd=f"{STORE_PATH}/{split}"
         )
         subprocess.run(
             f"rm {task}.{split}.squashfs",
-            shell=True,
-            capture_output=True, text=True, check=True,
+            shell=True, check=True,
             cwd=f"{STORE_PATH}/{split}"
         )
